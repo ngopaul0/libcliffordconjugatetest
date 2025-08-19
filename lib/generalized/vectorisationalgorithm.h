e211f97fbdc80f12d81a3c1f8a31c940305d918b
Paul Ngo <ngopauln@sfu.ca>
2025-08-19 16:24:20 -0700

    implement vec(X) operator + template expressions
    createXtransposeTensorI can be templated so that it can also
    take matrices with compile-time constant dimensions.

--- lib/generalized/vectorisationalgorithm.h
+++ lib/generalized/vectorisationalgorithm.h
@@ -2,36 +2,50 @@
 #define VECTORISATIONALGORITHM_H
 
 #include <Eigen/Dense>
-#include "internal/util.h"
-#include <unsupported/Eigen/KroneckerProduct>
+// ReSharper disable once CppUnusedIncludeDirective
+#include "unsupported/Eigen/KroneckerProduct"
 
 namespace cliffconjtest {
 
-inline Eigen::Matrix<long, Eigen::Dynamic, Eigen::Dynamic> createXtransposeTensorI(const Eigen::Matrix<long, Eigen::Dynamic, Eigen::Dynamic>& X) {
-    if (X.rows() % 2 != 0) {
-        throw std::invalid_argument("Vector size must be even");
-    }
-    const size_t n = X.rows() / 2;
+/**
+ * Computes X^T tensor I_m
+ * @tparam Derived The Derived Eigen matrix type
+ * @param X An m x n matrix representing a list of vectors
+ * @return X^T tensor I_m
+ */
+template <typename Derived>
+auto createXtransposeTensorI(const Eigen::MatrixBase<Derived>& X) {
+    // For the purposes of our algorithm, the columns of X should be vectors in Z_d^(2n).
+    assert(X.rows() % 2 != 0);
 
-    Eigen::Matrix<long, Eigen::Dynamic, Eigen::Dynamic> identity(X.rows(), X.rows());
-    for (size_t i = 0; i < identity.rows(); ++i) {
-        for (size_t j = 0; j < identity.cols(); ++j) {
-            if (i == j) {
-                identity(i, j) = 1;
-            } else {
-                identity(i, j) = 0;
-            }
-        }
-    }
+    using Scalar = typename Derived::Scalar;
+    const auto identity =
+        Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>::Identity(X.rows(), X.rows());
 
-    Eigen::Matrix<long, Eigen::Dynamic, Eigen::Dynamic> theResult = kroneckerProduct(X.transpose(), identity);
-    return theResult;
+    return kroneckerProduct(X.transpose(), identity);
 }
 
-//inline Eigen::Matrix<long, Eigen::Dynamic, Eigen::Dynamic> findSymplectic() {
+/**
+ * @brief If X = [ x1 ... xn ] is an m x n matrix (xi in F^m where F is any set of scalars), then
+ * vec(X) = <x1, x2, ..., xn> is a mn x 1 column vector with columns of X stacked on each other
+ *
+ * @tparam MatrixType The Eigen matrix type
+ * @param X The m x m matrix to apply the vectorise operator on
+ * @return mn x 1 column vector, vec(X)
+ */
+template <typename MatrixType>
+Eigen::Map<Eigen::Vector<typename MatrixType::Scalar, Eigen::Dynamic>> vecOperator(MatrixType& X) {
+    static_assert(!MatrixType::IsRowMajor,
+        "The matrix must be in column-major order for efficient vec operator.");
+    // Since Eigen matrices here are stored in column-major order, it's trivial to make the
+    // vec operator. Eigen::Map allows us to create a vector without any data being copied.
+    return Eigen::Map<Eigen::Vector<typename MatrixType::Scalar, Eigen::Dynamic>>(X.data(), X.size());
+}
+
+// inline Eigen::Matrix<long, Eigen::Dynamic, Eigen::Dynamic> findSymplectic() {
 
 //}
 
 } // namespace cliffconjtest
 
-#endif //VECTORISATIONALGORITHM_H
+#endif // VECTORISATIONALGORITHM_H
--- unittest/generalized/VectorisationAlgorithmTests.cpp
+++ unittest/generalized/VectorisationAlgorithmTests.cpp
@@ -13,31 +13,76 @@
 using namespace cliffconjtest;
 
 TEST_CASE("vectorisation", "[vectorisationalgorithm]") {
-    SECTION("Performs createXtransposeTensorI correctly") {
+    SECTION("Performs createXtransposeTensorI correctly for one vector") {
         const size_t d = 5;
         const size_t n = 1;
-        Eigen::Matrix<long, Eigen::Dynamic, Eigen::Dynamic> X(2*n, 1);
+        Eigen::Matrix<long, 2, 1> X;
         Eigen::Vector<long, 2> v;
         v << 1, 2;
         X.col(0) = v;
-        INFO("X = " << X);
+        // INFO("X = " << X);
+        REQUIRE(X == v);
         auto XtransposeTensorI = createXtransposeTensorI(X);
+        auto XtransposeTensorI_from_v = createXtransposeTensorI(v);
+        Eigen::Matrix<long, 2, 4> expectedXT_tensor_I;
+        expectedXT_tensor_I << 1, 0, 2, 0,
+                               0, 1, 0, 2;
+        REQUIRE(XtransposeTensorI == expectedXT_tensor_I);
+        REQUIRE(XtransposeTensorI_from_v == expectedXT_tensor_I);
         INFO("XtransposeTensorI = " << XtransposeTensorI);
-
-        Eigen::Matrix<long, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> S(2*n, 2*n);
+        Eigen::Matrix<long, Eigen::Dynamic, Eigen::Dynamic> S(2*n, 2*n);
         S << 2, 2, 0, 2;
 
-        Eigen::Vector<long, 2> Sv = S * v;
-        Sv = Sv.array().unaryExpr([&](const long x) { return safeMod(x, d); });
+        Eigen::Vector<long, 2> Sv = modMatrix(S * v, d);
 
         Eigen::Vector<long, 2> expectedSv(1, 4);
         REQUIRE(Sv == expectedSv);
 
-        Eigen::Vector<long, Eigen::Dynamic> vecS = Eigen::Map<Eigen::Vector<long, Eigen::Dynamic>>(S.data(), S.size());
-        REQUIRE(XtransposeTensorI.rows() == S.cols());
-        Eigen::Vector<long, Eigen::Dynamic> XtranposeTensorITimesVecS = XtransposeTensorI * vecS;
-        XtranposeTensorITimesVecS = XtranposeTensorITimesVecS.array().unaryExpr([&](const long x) { return safeMod(x, d); });
+        auto vecS = vecOperator(S);
+        REQUIRE(XtransposeTensorI.cols() == vecS.rows());
+        auto XtranposeTensorITimesVecS = modMatrix(XtransposeTensorI * vecS, d);
         INFO("XtranposeTensorITimesVecS = " << XtranposeTensorITimesVecS);
         REQUIRE(XtranposeTensorITimesVecS == expectedSv);
+    }
+
+    SECTION("Performs createXtransposeTensorI correctly for multiple vectors") {
+        const size_t d = 5;
+        const size_t n = 1;
+        Eigen::Matrix<long, 2, 2> X;
+
+        Eigen::Vector<long, 2> v1;
+        v1 << 1, 2;
+        X.col(0) = v1;
+        Eigen::Vector<long, 2> v2;
+        v2 << 2, 4;
+        X.col(1) = v2;
+
+        auto XtransposeTensorI = createXtransposeTensorI(X);
+        Eigen::Matrix<long, 4, 4> expectedXT_tensor_I;
+        expectedXT_tensor_I << 1, 0, 2, 0,
+                               0, 1, 0, 2,
+                               2, 0, 4, 0,
+                               0, 2, 0, 4;
+        REQUIRE(XtransposeTensorI == expectedXT_tensor_I);
+        INFO("XtransposeTensorI = " << XtransposeTensorI);
+        Eigen::Matrix<long, Eigen::Dynamic, Eigen::Dynamic> S(2*n, 2*n);
+        S << 2, 2,
+             0, 2;
+
+        auto SX = modMatrix(S * X, d);
+
+        Eigen::Matrix<long, 2, 2> expectedSX;
+        expectedSX << 1, 2,
+                      4, 3;
+        REQUIRE(SX == expectedSX);
+
+        Eigen::Map<Eigen::Vector<Eigen::Matrix<long, -1, -1>::Scalar, Eigen::Dynamic>> vecS =
+            vecOperator(S);
+        auto vecExpectedSX = vecOperator(SX);
+
+        REQUIRE(XtransposeTensorI.cols() == vecS.rows());
+        auto XtranposeTensorITimesVecS = modMatrix(XtransposeTensorI * vecS, d);
+        INFO("XtranposeTensorITimesVecS = " << XtranposeTensorITimesVecS);
+        REQUIRE(XtranposeTensorITimesVecS == vecExpectedSX);
     }
 }
